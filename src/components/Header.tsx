d4a5b8314fabeab16359589b2d4b70eedeffeb6f
Kaue Recio <kaue.recio2@gmail.com>
2022-08-24 19:51:45 -0300

    Header user interactions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri'
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header(){
   return (
@@ -41,7 +41,33 @@ export function Header(){
         />
         <Icon as={RiSearchLine} fontSize='20'/>
       </Flex>
+      <Flex
+        align='center'
+        ml='auto'
+      >
+        <HStack
+          spacing='8'
+          mx='8'
+          pr='8'
+          py='1'
+          color='gray.300'
+          borderRightWidth={1}
+          borderColor='gray.700'
+        >
+          <Icon as={RiNotificationLine} fontSize='20'/>
+          <Icon as={RiUserAddLine} fontSize='20'/>
+        </HStack>
 
+        <Flex align='center'>
+          <Box mr='4' textAlign='right'>
+            <Text>Kaue Recio</Text>
+            <Text color='gray.300' fontSize='small'>
+              kaue.recio2@gmail.com
+            </Text>
+          </Box>
+          <Avatar size='md' name="Kaue Recio" src="https://github.com/reciokaue.png"/>
+        </Flex>
+      </Flex>
     </Flex>
   )
 }
