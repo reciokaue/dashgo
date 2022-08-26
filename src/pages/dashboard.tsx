8d52de41e331d020b1f616617243791144d66cd3
Kaue Recio <kaue.recio2@gmail.com>
2022-08-26 16:23:46 -0300

    Pagination

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -62,13 +62,7 @@ export default function Dashboard(){
   return (
     <Flex direction='column' h='100vh'>
       <Header/>
-      <Flex
-        w='100%'
-        my='6'
-        maxW={1480}
-        mx='auto'
-        px='6'
-      >
+      <Flex w='100%' my='6' maxW={1480} mx='auto' px='6'>
         <Sidebar/>
         <SimpleGrid flex='1' gap='4' minChildWidth='320px' alignContent='flex-start'>
           <Box
